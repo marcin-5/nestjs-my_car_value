e992117f4f915672c410fcef767c6dfcd6db4ca8
Marcin Bojara <marcin_5@mail.ru>
2024-01-08 18:49:22 +0100

    get rid of any type annotations

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,16 +8,20 @@ import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export function Serialize(dto: any) {
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
-      map((data: any) => {
+      map((data: ClassConstructor) => {
         return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         });
