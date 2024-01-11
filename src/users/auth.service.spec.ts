8142bf0f12a5a0ac70ca8e7e250d15ae66f3342a
Marcin Bojara <marcin_5@mail.ru>
2024-01-11 19:00:55 +0100

    test unused email

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -47,6 +47,12 @@ describe('AuthService', () => {
       Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
+    );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(service.signin('asdflkj@asdlfkj.com', 'passdflkj')).rejects.toThrow(
+      NotFoundException,
     );
   });
 });
