cddcd5baf6884a57f91f6576ccf602449c3101ec
Marcin Bojara <marcin_5@mail.ru>
2024-01-13 12:58:33 +0100

    fix type definitio error

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,17 +1,21 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { User } from '../user.entity';
 import { UsersService } from '../users.service';
+
+interface CustomRequest extends Request {
+  currentUser?: User;
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: CustomRequest, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // @ts-ignore
       req.currentUser = user;
     }
 
