24978e403e5a1d06a037aed783eaf8bdfecc9069
Marcin Bojara <marcin_5@mail.ru>
2024-01-13 15:43:41 +0100

    transform query string data

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -1,4 +1,5 @@
-import { IsString, IsNumber, Min, Max, IsLongitude, IsLatitude } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class GetEstimateDto {
   @IsString()
@@ -7,19 +8,23 @@ export class GetEstimateDto {
   @IsString()
   model: string;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number;
 }
